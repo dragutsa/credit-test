f85285a7d4e4ae8fddc4a409339a901644da9c3e
Denis Draguta <d.draguta@sxope.com>
2025-03-04 12:38:40 +0200

    init

--- src/Credit/Domain/Deciders/Decider.php
+++ src/Credit/Domain/Deciders/Decider.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Credit\Domain\Deciders;
 
-/** Decorators place */
+/**
+ * Decorators or Visitor
+ */
 interface Decider
 {
     public function decide(): bool;
--- src/Credit/Domain/Deciders/RandomDecider.php
+++ src/Credit/Domain/Deciders/RandomDecider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Credit\Domain\Deciders;
 
+/**
+ * Decorators or Visitor
+ */
 final readonly class RandomDecider implements Decider
 {
     public function decide(): bool
